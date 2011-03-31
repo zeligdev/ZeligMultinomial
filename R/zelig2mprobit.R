0ad2af12dc29de8d4af6e0fdd0eee2ebbafc9c71
Matt Owen <mowen@iq.harvard.edu>
2011-03-31 18:04:31 -0400

    Adding comments

--- R/zelig2mprobit.R
+++ R/zelig2mprobit.R
@@ -23,8 +23,10 @@ zelig2mprobit <- function (formula, choiceX = NULL, cXnames = NULL, ..., data) {
        )
 }
 
-#' Hook
-#'
+#' Hook to Clean-up the Function Signature
+#' This is _NOT_ cosmetic! MNP evaluates choiceX
+#' by looking at the call signature. This is necessary
+#' to make the model work with the function predict.mnp
 update.call.formula <- function(obj, zelig.call, call) {
   obj$call$formula <- as.formula(call$formula)
   obj$call$choiceX <- call$choiceX
